84077d5a99e7d2bfe0740afdad9eb2b057d8cef7
rukonit <rukon-it@live.com>
2022-02-24 14:50:36 -0500

    completed

---
+++ README.md
@@ -0,0 +1,22 @@
+# React Food Order SPA
+
+This react project is a simple demonstraton of online food ordering service. Backend of this app uses Firebase realtime database.
+
+
+### How to Run
+
+Runs the app in the development mode.\
+Open https://book-management-front-end-rukon.netlify.app to view it in the browser.
+
+### Technology Used
+1. ReactJS
+2. JavaScript
+3. HTML
+4. CSS
+
+### App Interface
+User should be able to add items to the cart, and place an order which will be received by the firebase backend.
+
+
+
+The app is expected to be developed further.
--- src/components/BasicForm.js
+++ src/components/BasicForm.js
@@ -1,22 +1,81 @@
+import useInput from "../hooks/use-input";
+
+
+
 const BasicForm = (props) => {
+  const {
+    value: enteredName,
+    isValid: nameIsValid,
+    hasError: nameHasError,
+    valueChangedHandler: nameChangedHandler,
+    inputBlurHandler: nameInputBlurHandler,
+    reset: nameReset
+} = useInput(name => name.trim() !== '')
+
+const {
+  value: enteredLastName,
+  isValid: lastNameIsValid,
+  hasError: lastNameHasError,
+  valueChangedHandler: lastNameChangedHandler,
+  inputBlurHandler: lastNameInputBlurHandler,
+  reset: lastNameReset
+} = useInput(name => name.trim() !== '')
+
+const {
+  value: enteredEmail,
+  isValid: emailIsValid,
+  hasError: emailHasError,
+  valueChangedHandler: emailChangedHandler,
+  inputBlurHandler: emailInputBlurHandler,
+  reset: emailReset
+} = useInput(name => name.trim().includes('@'))
+
+  let formIsValid = false;
+
+  if (nameIsValid && lastNameIsValid && emailIsValid) {
+    formIsValid = true
+  }
+  const submitHandler = (event) => {
+    event.preventDefault()
+
+    if(nameHasError && lastNameHasError) {
+      return; 
+    }
+
+    console.log("name: " + enteredName + " latname: " + enteredLastName+ " email: " + enteredEmail);
+    nameReset()
+    lastNameReset()
+    emailReset()
+    formIsValid = false
+  }
+
+  const nameInputClass = !nameHasError ? 'form-control' : 'form-control invalid'
+
+  const lastNameInputClass = !lastNameHasError ? 'form-control' : 'form-control invalid'
+
+  const emailInputClass = !emailHasError ? 'form-control' : 'form-control invalid'
+
   return (
-    <form>
+    <form onSubmit={submitHandler}>
       <div className='control-group'>
-        <div className='form-control'>
+        <div className={nameInputClass}>
           <label htmlFor='name'>First Name</label>
-          <input type='text' id='name' />
+          <input onChange={nameChangedHandler} onBlur={nameInputBlurHandler} type='text' id='name' value={enteredName} />
+          {nameHasError && <p className="error-text">Please enter a valid name</p>}
         </div>
-        <div className='form-control'>
+        <div className={lastNameInputClass}>
           <label htmlFor='name'>Last Name</label>
-          <input type='text' id='name' />
+          <input onChange={lastNameChangedHandler} onBlur={lastNameInputBlurHandler} type='text' id='name' value={enteredLastName}/>
+          {lastNameHasError && <p className="error-text">Please enter a valid last name</p>}
         </div>
       </div>
-      <div className='form-control'>
+      <div className={emailInputClass}>
         <label htmlFor='name'>E-Mail Address</label>
-        <input type='text' id='name' />
+        <input onChange={emailChangedHandler} onBlur={emailInputBlurHandler} type='text' id='name' value={enteredEmail} />
+        {emailHasError && <p className="error-text">Please enter a valid email</p>}
       </div>
       <div className='form-actions'>
-        <button>Submit</button>
+        <button disabled={!formIsValid}>Submit</button>
       </div>
     </form>
   );
